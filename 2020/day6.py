22478c39d1a0ebd9c07ee16ea676d1b4cbdfdd40
Gnomeball <gnome.c@protonmail.com>
2020-12-06 14:46:22 +0000

    Day 6

---
+++ 2020/day6.py
@@ -0,0 +1,14 @@
+with open("day6.txt", "r") as file:
+    answers = file.read().split("\n\n")
+
+# Part 1
+
+counts = [len(set(a)) for a in [a.replace("\n", "") for a in answers]]
+
+print(sum(counts))
+
+# Part 2
+
+counts = [len(set.intersection(*a)) for a in [[set(sorted(e)) for e in a] for a in [a.split("\n") for a in answers]]]
+
+print(sum(counts))
