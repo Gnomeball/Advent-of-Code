c8dad21616144a40a88596b4e2130a1ba5ad7e04
Gnomeball <gnome.c@protonmail.com>
2020-12-13 17:31:56 +0000

    Days 11 and 12, Part 1

---
+++ 2020/day11.py
@@ -0,0 +1,50 @@
+with open("day11.txt", "r") as file:
+    seats = [[seat for seat in row] for row in file.read().splitlines()]
+
+HEIGHT = len(seats)
+WIDTH = len(seats[0])
+
+EMPTY_SEAT = "L"
+FLOOR = "."
+OCCUPIED_SEAT = "#"
+
+def getNeighbours(thing, x, y):
+    neighbours = []
+    for i, j in [(0,1), (1,0), (0,-1), (-1,0), (1,1), (1,-1), (-1,-1), (-1,1)]:
+        this_x, this_y = x + i, y + j
+        if not (this_x < 0 or this_x >= WIDTH or this_y < 0 or this_y >= HEIGHT):
+            neighbours.append(thing[this_y][this_x])
+    return neighbours
+
+def updateSeat(thing, x, y):
+    seat = thing[y][x]
+    neighbours = getNeighbours(thing, x, y)
+    if seat == EMPTY_SEAT:
+        return OCCUPIED_SEAT if OCCUPIED_SEAT not in neighbours else EMPTY_SEAT
+    elif seat == OCCUPIED_SEAT:
+        return EMPTY_SEAT if neighbours.count(OCCUPIED_SEAT) >= 4 else OCCUPIED_SEAT
+    else:
+        return seat
+
+def updateSeats(thing):
+    next = [[seat for seat in row] for row in thing]
+    for y in range(HEIGHT):
+        for x in range(WIDTH):
+            next[y][x] = updateSeat(thing, x, y)
+    return next
+
+def countOccupiedSeats(thing):
+    return sum(row.count('#') for row in thing)
+
+def partOne():
+    next = [[seat for seat in row] for row in seats]
+    while True:
+        last = [[seat for seat in row] for row in next]
+        next = updateSeats(last)
+        if last == next:
+            break
+    print(countOccupiedSeats(next))
+
+# Part 1
+
+partOne()
---
+++ 2020/day12.py
@@ -0,0 +1,22 @@
+with open("day12.txt", "r") as file:
+    directions = [(d[0], int(d[1:])) for d in file.read().splitlines()]
+
+headings_d = ["N", "E", "S", "W"]
+headings_v = [0, 0, 0, 0]
+
+heading_c = "E"
+
+for direction in directions:
+    d = direction[0]
+
+    if d in headings_d:
+        headings_v[headings_d.index(d)] += direction[1]
+
+    elif d == "F":
+        headings_v[headings_d.index(heading_c)] += direction[1]
+
+    else:
+        c = [1, -1][["R", "L"].index(d)]
+        heading_c = headings_d[int((headings_d.index(heading_c) + (c * (direction[1]) / 90)) % 4)]
+
+print(abs(headings_v[0] - headings_v[2]) + abs(headings_v[1] - headings_v[3]))
