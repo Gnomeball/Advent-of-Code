8ea355bdc5b173eb6bc922eb707202f71cd47093
Gnomeball <gnome.c@protonmail.com>
2021-11-28 02:03:02 +0000

    Ok now we take a break from 2015

--- 2015/day20.rb
+++ 2015/day20.rb
@@ -10,9 +10,8 @@ def factor_sum(n)
     return factors(n).sum * 10 # (because puzzle)
 end
 
-# Number derived from target / [1..10].sum
-# Then round up a lot to a factor of 560
-# (560 divides evenly by all of [1..10])
+# Number derived from target / [1..10].sum then round up a lot
+# to a factor of 560 (560 divides evenly by all of [1..10])
 # In particular 1400 * 560
 start = 784000
 
@@ -25,6 +24,8 @@ while true
     # We then only need to check values that divide by 560
     start += 560
 end
+
+# Yes this is slow, but is faster than the below approach with the asumptions
 
 # === part 2 ===
 
