9ad3e7d22d0b4232427f69c8c25bef38bdab59b5
Gnomeball <gnome.c@protonmail.com>
2021-12-10 20:43:04 +0000

    Pls

---
+++ 2021/day09.rb
@@ -0,0 +1,67 @@
+input = File.readlines("day09.txt", chomp: true).map { |l| l.split("").map(&:to_i) }
+
+for l in input
+    l.unshift(9)
+    l.push(9)
+end
+
+input.unshift(Array.new(input[0].length) { 9 } )
+input.push(Array.new(input[0].length) { 9 } )
+
+def neighbours(x, y)
+    return [ [x, y-1], [x+1, y], [x, y+1], [x-1, y] ]
+end
+
+low_points = []
+
+for y in 1...input.length-1
+    for x in 1...input[0].length-1
+        low = neighbours(x, y).map { |n| input[y][x] < input[n[1]][n[0]] }.reduce(&:&)
+        if low then
+            low_points.push([x, y])
+        end
+    end
+end
+
+puts("part one = #{low_points.map { |l| input[l[1]][l[0]] + 1 }.sum}\n")
+
+$visited = Array.new(input.length) { Array.new(input[0].length) { false } }
+
+def basin(grid, y, x)
+
+    if $visited[y][x] == true then
+        # print "this is fucking dumb", "\n"
+        return 0
+    end
+
+    if grid[y][x] == 9 then
+        # print "this is also fucking dumb", "\n"
+        $visited[y][x] = true
+        return 0
+    end
+
+    if x == 0 || y == 0 || x == grid.length-1 || y == grid[0].length-1 then
+        # print "this is fucking dumb too", "\n"
+        $visited[y][x] = true
+        return 0
+    end
+
+    $visited[y][x] = true
+
+    up = basin(grid, y-1, x)
+    dn = basin(grid, y+1, x)
+    lf = basin(grid, y, x-1)
+    rt = basin(grid, y, x+1)
+    return 1 + up + dn + lf + rt
+
+end
+
+basins = []
+
+for l in low_points
+    b = basin(input, l[1], l[0])
+    # print input[l[1]][l[0]], ", ", b, "\n"
+    basins.push(b)
+end
+
+print("part two = #{basins.sort[-3..].reduce(&:*)}\n")
---
+++ 2021/day10.rb
@@ -0,0 +1,83 @@
+input = File.readlines("day10.txt", chomp: true).map { |l| l.split("") }
+
+$corrupt_scores = {
+    ")" => 3,
+    "]" => 57,
+    "}" => 1197,
+    ">" => 25137
+}
+
+$incomplete_score = {
+    ")" => 1,
+    "]" => 2,
+    "}" => 3,
+    ">" => 4
+}
+
+$chars = {
+    "(" => ")",
+    "[" => "]",
+    "{" => "}",
+    "<" => ">"
+}
+
+def score_corrupted(line)
+    stack = []
+    for c in line
+        if "([{<".include?(c) then
+            stack.push(c)
+        else # * it's a closing
+            if $chars.fetch(stack.last) == c then
+                stack.pop
+            else
+                return $corrupt_scores.fetch(c)
+            end
+        end
+    end
+    return 0
+end
+
+part_one = 0
+incomplete_lines = []
+
+for i in input
+    score = score_corrupted(i)
+    part_one += score
+    if score == 0 then incomplete_lines.push(i) end
+end
+
+puts("part one = #{part_one}")
+
+def score_incomplete(line)
+    stack = []
+    for c in line
+        if "([{<".include?(c) then
+            stack.push(c)
+        else # * it's a closing
+            if $chars.fetch(stack.last) == c then
+                stack.pop
+            else
+                return "PANIC on " + line
+            end
+        end
+    end
+    score = 0
+    for char in stack.reverse
+        c = $chars.fetch(char)
+        score *= 5
+        score += $incomplete_score.fetch(c)
+    end
+    return score
+end
+
+part_two = []
+
+for i in incomplete_lines
+    score = score_incomplete(i)
+    part_two.push(score)
+end
+
+part_two.sort!
+result = part_two[(part_two.length-1)/2]
+
+puts("part two = #{result}")
