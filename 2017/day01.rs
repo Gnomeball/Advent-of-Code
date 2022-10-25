2c2750fd2a3f7e34a7e65015cce6488889752a43
Gnomeball <gnome.c@protonmail.com>
2022-10-25 18:01:01 +0100

    2017 practice Might be a gogo

---
+++ 2017/day01.rs
@@ -0,0 +1,76 @@
+use std::fs;
+
+fn main() {
+    let data = fs::read_to_string("data/day01.txt").unwrap();
+
+    println!("part one: {}", part_one(&data.as_bytes()));
+    println!("part two: {}", part_two(&data.as_bytes()));
+}
+
+fn part_one(data: &[u8]) -> u32 {
+    let mut count: u32 = 0;
+
+    for i in 0..data.len()-1 {
+        if data[i] == data[i+1] {
+            count += (data[i] as u32) - 48;
+        }
+    }
+
+    if data[0] == data[data.len()-1] {
+        count += (data[0] as u32) - 48;
+    }
+
+    return count;
+}
+
+fn part_two(data: &[u8]) -> u32 {
+    let mut count: u32 = 0;
+
+    for i in 0..data.len() {
+        let mut other_index = i + data.len() / 2;
+        if other_index >= data.len() { other_index -= data.len() };
+        let other = data[other_index];
+
+        if data[i] == other {
+            count += (data[i] as u32) - 48;
+        }
+    }
+
+    return count;
+}
+
+#[test]
+fn test_part_one() {
+
+    let tests = [
+        ("1122", 3),
+        ("1111", 4),
+        ("1234", 0),
+        ("91212129", 9)
+    ];
+
+    for (data, expect) in tests {
+        let result = part_one(&data.as_bytes());
+        assert_eq!(result, expect);
+    }
+
+}
+
+#[test]
+fn test_part_two() {
+
+    let tests = [
+        ("1212", 6),
+        ("1221", 0),
+        ("123425", 4),
+        ("123123", 12),
+        ("12131415", 4)
+    ];
+
+    for (data, expect) in tests {
+        println!("Testing {} {}", data, expect);
+        let result = part_two(&data.as_bytes());
+        assert_eq!(result, expect);
+    }
+
+}
