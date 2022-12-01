74236b05b30a88e2083ad52c57bcf3a26feffe08
Gnomeball <gnome.c@protonmail.com>
2022-12-01 21:32:16 +0000

    2020 Day 7 Part 2 actually done..

--- 2020/day07.py
+++ 2020/day07.py
@@ -1,22 +1,22 @@
 from parse import parse
 
 with open("data/day07.txt", "r") as file:
-    rules = dict([(r,[(int(b.split()[0])," ".join(b[2:].split()[:-1])) for b in b.split(", ")] if b != "no other bags" else []) for r,b in map(lambda l: parse("{} bags contain {}", l[:-1]).fixed, file.read().splitlines())])
+    rules = dict([(r,[(" ".join(b[2:].split()[:-1]), int(b.split()[0])) for b in b.split(", ")] if b != "no other bags" else []) for r,b in map(lambda l: parse("{} bags contain {}", l[:-1]).fixed, file.read().splitlines())])
 
-def get_candidates(bag):
-    return [rule for rule in rules if bag in [bag[1] for bag in rules[rule]]]
+def bags_that_can_hold(bag):
+    candidates = []
+    for rule in rules:
+        for bags in rules[rule]:
+            if bag == bags[0]:
+                candidates.append(rule)
+                candidates += bags_that_can_hold(rule)
+    return set(candidates)
 
-candidates = get_candidates("shiny gold")
+def contains_how_many_bags(bag):
+    bags_this_contains = 1
+    for bags in rules[bag]:
+        bags_this_contains += bags[1] * contains_how_many_bags(bags[0])
+    return bags_this_contains
 
-last, curr = 0, 1
-
-while last != curr:
-    last = curr
-    for bag in candidates:
-        new = get_candidates(bag)
-        candidates += new
-    curr = len(set(candidates))
-
-print(f"Part one = {len(set(candidates))}")
-
-# print(f"Part two = {}")
+print("Part one = {}".format(len(bags_that_can_hold("shiny gold"))))
+print("Part one = {}".format(contains_how_many_bags("shiny gold") - 1))
