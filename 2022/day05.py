64f845afdf5e0ecb0a6438a8b9abb2daa61794f5
Gnomeball <gnome.c@protonmail.com>
2022-12-05 17:31:21 +0000

    Messy messy messy

--- 2022/day05.py
+++ 2022/day05.py
@@ -2,55 +2,56 @@ from parse import *
 
 def get_stacks():
 
-    def build_stacks(stacks_in):
-        # I don't like this
-        stacks_out = [[], [], [], [], [], [], [], [], [], []]
-        for stack in stacks_in:
-            i, s = 1, 0
-            while i < width:
-                s += 1 # Next stack
-                container = stack[i]
-                i += 4
-                if container == ' ': continue
-                else: stacks_out[s].insert(0, container)
-        # We don't pop this to save us from off by one errors later
-        # stacks_out.pop(0)
-        return stacks_out
-
     with open("data/day05.txt") as file:
         data = file.read().splitlines()
-        # I don't like how these are hard-coded
-        width  = len(data[7])
-        stacks = [ stack.ljust(width) for stack in data[:8] ]
-        moves  = [ parse("move {:d} from {:d} to {:d}", move) for move in data[10:] ]
+        # gap = data.index("")
 
-    stacks = build_stacks(stacks)
-    return stacks, moves
+        # v_stacks = [ stack.ljust(len(data[gap-2])) for stack in data[:gap] ]
+        # h_stacks = [ list(reversed(stack)) for stack in list(zip(*v_stacks)) ]
+        # stacks = [ [ c for c in "".join(h_stacks[i])[1:].strip() ] for i in range(1, len(data[gap-2]), 4) ]
 
-def read_stacks(stacks):
-    return "".join(stack[len(stack)-1] for stack in stacks[1:])
+    # return stacks, moves
+
+    # return [ [ c for c in "".join([ list(reversed(stack)) for stack in list(zip(*[ stack.ljust(len(data[data.index("")-2])) for stack in data[:data.index("")] ])) ][i])[1:].strip() ] for i in range(1, len(data[data.index("")-2]), 4) ], [ parse("move {:d} from {:d} to {:d}", move) for move in data[data.index("")+1:] ]
+
+    return [ [ c for c in "".join([ list(reversed(stack))
+                 for stack in list(zip(*[ stack.ljust(len(data[data.index("")-2]))
+                 for stack in data[:data.index("")] ])) ][i])[1:].strip()
+             ]   for i in range(1, len(data[data.index("")-2]), 4)
+           ], [
+             parse("move {:d} from {:d} to {:d}", move)
+             for move in data[data.index("")+1:]
+           ]
+
+
+
+# def read_stacks(stacks):
+#     return "".join(stack[len(stack)-1] for stack in stacks[1:])
 
 def part_one(stacks, moves):
 
-    for move in moves:
-        for _ in range(move[0]):
-            container = stacks[move[1]].pop()
-            stacks[move[2]].append(container)
-        # [ stacks[move[2]].append(stacks[move[1]].pop()) for _ in range(move[0]) ]
+    # for move in moves:
+    #     for _ in range(move[0]):
+    #         container = stacks[move[1]].pop()
+    #         stacks[move[2]].append(container)
 
-    return read_stacks(stacks)
+    [ [ stacks[move[2]-1].append(stacks[move[1]-1].pop()) for _ in range(move[0]) ] for move in moves ]
+
+    # return read_stacks(stacks)
+    return "".join(stack[len(stack)-1] for stack in stacks)
 
 def part_two(stacks, moves):
 
     for move in moves:
-        pile = []
-        for _ in range(move[0]):
-            container = stacks[move[1]].pop()
-            pile.insert(0, container)
-        stacks[move[2]] += pile
-        # stacks[move[2]] += reversed([ stacks[move[1]].pop() for _ in range(move[0]) ])
+        # pile = []
+        # for _ in range(move[0]):
+        #     container = stacks[move[1]].pop()
+        #     pile.insert(0, container)
+        # stacks[move[2]] += pile
+        stacks[move[2]-1] += reversed([ stacks[move[1]-1].pop() for _ in range(move[0]) ])
 
-    return read_stacks(stacks)
+    # return read_stacks(stacks)
+    return "".join(stack[len(stack)-1] for stack in stacks)
 
 print(f"Part one = {part_one(*get_stacks())}")
 print(f"Part two = {part_two(*get_stacks())}")
